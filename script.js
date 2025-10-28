ba3f3387cf90686ef6d3b6b5816016cb368ef318
koscinskimateusz <177106@stud.prz.edu.pl>
2025-10-28 08:57:12 +0000

    Com2_2.3

--- script.js
+++ script.js
@@ -4,11 +4,16 @@
   const cw2 = document.getElementById('cw2')
   const cw3 = document.getElementById('cw3')
   const answer = document.getElementById('answer')
+  const loadingPopup = document.getElementById('loading-popup')
 
   example.addEventListener("click", function() {
+    loadingPopup.classList.add('show');
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
       .then(array => {
+        setTimeout(() => {
+          loadingPopup.classList.remove('show');
+        }, 1500);
         console.log(array)
         let tableHTML = '<table><thead><tr><th>ID</th><th>Tytuł</th><th>Treść</th></tr></thead><tbody>';
         array.forEach(post => {
